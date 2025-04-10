69d7dec8cec51c9eca9a91eb1f31e91160a14cdb
Pramod Rajane <pramod.rajane@gmail.com>
2025-04-10 13:46:07 +0530

    Initial commit

---
+++ graphqldemo/src/main/java/com/graphql/java/resolver/Resolver.java
@@ -0,0 +1,11 @@
+package com.graphql.java.resolver;
+
+import org.springframework.stereotype.Service;
+
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.graphql.java.query.Query;
+
+@Service
+public class Resolver implements GraphQLResolver<Query> {
+
+}
